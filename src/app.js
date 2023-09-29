d9fbf398e848a7868347d44828cf205bef675820
yonathanbaez25 <yacevedo170@gmail.com>
2023-09-29 09:54:16 -0400

    update routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const usersRoute = require("./routes/users");
 const authRouter = require("./routes/auth");
@@ -15,6 +16,7 @@ dotenv.config();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(cors());
 
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRouter);
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -80,9 +80,9 @@ router.get("/:id", async (req, res) => {
 });
 
 //get timeline posts
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
@@ -90,7 +90,7 @@ router.get("/timeline/all", async (req, res) => {
       })
     );
 
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     res.status(500).json(error);
   }
